3d78874f9a242c8b4aa548b8469a9a88416a1c49
lowercase-tousif <toisiftasrik@gmail.com>
2025-09-26 11:58:21 +0600

    Completed the header component

--- App.js
+++ App.js
@@ -1,3 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import logo from "./public/assets/images/logo.png";
+
+// Header Component
+const Header = () => {
+  console.log(logo);
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=food&sf=&txt_keyword=All"
+          alt="Logo"
+        />
+      </div>
+
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Contact us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+// AppLayout
+
+const AppLayout = () => {
+  return (
+    <div id="app">
+      <Header />
+    </div>
+  );
+};
+
+// React - Root
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
