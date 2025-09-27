b79a4054039959b061626cce5fd5b046c128a6b2
lowercase-tousif <toisiftasrik@gmail.com>
2025-09-27 21:00:20 +0600

    feat: Added search, body, A restaurant card

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-
 // Header Component
 const Header = () => {
   return (
@@ -9,7 +8,7 @@ const Header = () => {
       <div className="logo-container">
         <img
           className="logo"
-          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=food&sf=&txt_keyword=All"
+          src="https://foodibd.com/_next/static/media/logo.c6a0f759.svg"
           alt="Logo"
         />
       </div>
@@ -26,12 +25,50 @@ const Header = () => {
   );
 };
 
+// Restaurant Card
+const RestaurantCard = () => {
+  return (
+    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <img
+        className="res-card-image"
+        src="https://imrs.foodibd.com/api/v1/image-resize?imageUrl=https%3A%2F%2Fs3.ap-southeast-1.amazonaws.com%2Fcdn.foodibd.com%2Frestaurant-service%2Fkacchi-bhai-app-cover-d84f-20250430060533161.jpg&width=400"
+        alt=""
+      />
+      <div className="res-card-content">
+        <h3>Kacchi Bhai - Gulshan 2</h3>
+        <p>Biriyani, Puran Dhaka, Kacchi</p>
+        <p>4.4 stars</p>
+        <p>40 minutes</p>
+      </div>
+    </div>
+  );
+};
+
+// Body
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="Search">Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
 // AppLayout
 
 const AppLayout = () => {
   return (
     <div id="app">
       <Header />
+      <Body />
     </div>
   );
 };
