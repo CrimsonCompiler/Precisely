e95fbb40be981b6596ec9102c5ef5fb98968fbae
lowercase-tousif <toisiftasrik@gmail.com>
2025-09-30 18:12:29 +0600

    feat: Top rated restaurant can be filtered

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,7 +4,9 @@ import RestaurantCard from "./RestaurantCard";
 const Body = () => {
   return (
     <div className="body">
-      <div className="Search">Search</div>
+      <div className="filter">
+        <button className="filter-btn">Top Rated Restaurant</button>
+      </div>
       <div className="res-container">
         {resList.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
