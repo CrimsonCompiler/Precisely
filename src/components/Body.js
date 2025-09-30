fd68cdf287279b074391d8393a569cfa9c928097
lowercase-tousif <toisiftasrik@gmail.com>
2025-09-30 20:19:48 +0600

    Learned: Usestate and React Fiber

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,28 @@
+import { useState } from "react";
 import resList from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
 
 const Body = () => {
+  const [listOfTopRatedRestaurant, setListOfTopRatedRestaurant] = useState(resList);
+
   return (
     <div className="body">
       <div className="filter">
-        <button className="filter-btn">Top Rated Restaurant</button>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfTopRatedRestaurant.filter(
+              (list) => list.info.avgRating > 4
+            );
+
+            setListOfTopRatedRestaurant(filteredList);
+          }}
+        >
+          Top Rated Restaurant
+        </button>
       </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {listOfTopRatedRestaurant.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
