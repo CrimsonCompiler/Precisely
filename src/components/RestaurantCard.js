c3b208ae5b52f737bdf75b9a0054497b2428e9dc
lowercase-tousif <toisiftasrik@gmail.com>
2025-09-30 17:23:57 +0600

    Re-organized with proper folders an file name

---
+++ src/components/RestaurantCard.js
@@ -0,0 +1,28 @@
+import { CDN_URL } from "../utils/constants";
+
+// Restaurant Card
+const RestaurantCard = (props) => {
+  const{resData} = props;
+  const{deliveryTime} = resData.info.sla;
+  const { name, cuisines, costForTwo, cloudinaryImageId} = resData?.info;
+  return (
+    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <img
+        className="res-card-image"
+        src={
+           CDN_URL +
+          cloudinaryImageId
+        }
+        alt=""
+      />
+      <div className="res-card-content">
+        <h3>{name}</h3>
+        <p>{cuisines.join(", ")}</p>
+        <p>${costForTwo / 100}</p>
+        <p>{deliveryTime} minutes</p>
+      </div>
+    </div>
+  );
+};
+
+export default RestaurantCard;
---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+export const CDN_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const LOGO_URL = "https://foodibd.com/_next/static/media/logo.c6a0f759.svg";
