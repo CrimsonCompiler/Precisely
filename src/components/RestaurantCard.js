34408ecd8e4285f17976725ecd0983ce489e45bc
CrimsonCompiler <toisiftasrik@gmail.com>
2025-10-14 20:23:50 +0600

    Restaurant properties and everything changed according to the api

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,26 +1,16 @@
-import { CDN_URL } from "../utils/constants";
-
 // Restaurant Card
 const RestaurantCard = (props) => {
-  const{resData} = props;
-  const{deliveryTime} = resData.info.sla;
-  const { name, cuisines, costForTwo, cloudinaryImageId, avgRating} = resData?.info;
+  const { resData } = props;
+  const { name, rating, hero_listing_image, characteristics } = resData;
   return (
     <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
-      <img
-        className="res-card-image"
-        src={
-           CDN_URL +
-          cloudinaryImageId
-        }
-        alt=""
-      />
+      <img className="res-card-image" src={hero_listing_image} alt="" />
       <div className="res-card-content">
         <h3>{name}</h3>
-        <p>{cuisines.join(", ")}</p>
-        <p>{costForTwo} </p>
-        <p>{deliveryTime} minutes</p>
-        <p>{avgRating} rating</p>
+        <p>
+          {characteristics.cuisines.map((cuisine) => cuisine.name).join(", ")}
+        </p>{" "}
+        <p>{rating} rating</p>
       </div>
     </div>
   );
