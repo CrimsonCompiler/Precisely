4ba7775e993f7b512f7f4c296062bea3033608e1
CrimsonCompiler <toisiftasrik@gmail.com>
2025-10-14 20:21:45 +0600

    Added a new Restaurant Details component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import About from "./components/About";
 import Error from "./components/Error";
 import Contactus from "./components/Contactus";
+import RestaurantDetails from "./components/RestaurantDetails";
 
 const AppLayout = () => {
   return (
@@ -29,6 +30,10 @@ const appRouter = createBrowserRouter([
       {
         path: "/contact",
         element: <Contactus />,
+      },
+      {
+        path: "/restaurant-details/:resId",
+        element: <RestaurantDetails />,
       },
     ],
     errorElement: <Error />,
