50b1089c179fdf876f6ed41125691040cb90a10b
CrimsonCompiler <toisiftasrik@gmail.com>
2025-10-14 10:23:16 +0600

    Learned about the Link | difference between the hyperlink and Link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
+import { Link } from "react-router-dom";
 
 // Header Component
 const Header = () => {
@@ -12,9 +13,15 @@ const Header = () => {
 
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About us</li>
-          <li>Contact us</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
           <li>Cart</li>
           <button
             onClick={() => {
